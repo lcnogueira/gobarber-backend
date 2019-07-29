a114ef287afe5fb6516f11ed0bc06f09da988706
Luiz Claudio <luizcns18@gmail.com>
2019-07-29 20:48:40 -0300

    feat: Load user model

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Rocketseat' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Luiz Claudio',
+    email: 'luizcns18@gmail.com',
+    password_hash: '34lkj23l4kj3kl',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
