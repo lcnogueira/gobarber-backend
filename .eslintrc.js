813e9ad653adcf4f0b589ab7681cb065f8ddf657
Luiz Claudio <luizcns18@gmail.com>
2019-09-14 09:30:04 -0300

    config: Set jest config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'jest'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
