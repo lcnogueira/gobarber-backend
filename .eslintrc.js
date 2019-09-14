3bdf3ddf7a68d81e3f3e5c8214900632fcf3ba2d
Luiz Claudio <luizcns18@gmail.com>
2019-09-14 10:49:14 -0300

    config: Set eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  env: {
+    "jest/globals": true
+  },
   rules: {
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
